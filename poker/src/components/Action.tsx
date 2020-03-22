ff9816fc302a67c3ddaef6745dd870301d58672b
Justin May <justinmay@Justins-MacBook-Pro.local>
2020-03-22 12:11:29 -0400

    added action buttons

--- poker/src/components/Action.tsx
+++ poker/src/components/Action.tsx
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import '../stylesheets/Action.css';
 
 type ActionProps = {
 };
 type ActionState = {
-  count: number; // like this
+  bet: number; // like this
 };
 
 class Action extends React.Component<ActionProps, ActionState> {
-  state: ActionState = {
-    // optional second annotation for better type inference
-    count: 0
-  };
+    constructor(props: ActionProps) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    state: ActionState = {
+        // optional second annotation for better type inference
+        bet: 0
+    };
 
-  render() {
+    handleChange(event: ChangeEvent<HTMLInputElement>) {
+        let bet = event.target.value;
+        if (bet.match(/^-{0,1}\d+$/)) {
+            this.setState({
+                bet: parseInt(bet)
+            })
+        } else if (bet === "") {
+            this.setState({
+                bet: 0
+            })
+        }
+        
+    }
+
+    render() {
     return (
         <div className="actionRow">
-            <button className="actionButton">
+            <button className="actionButton greenBack">
                 <p>
-                    Buy
+                    Check
                 </p>
             </button>
+            <button className="actionButton long blueBack">
+                <p>
+                    Check > Fold
+                </p>
+            </button>
+            <button className="actionButton redBack">
+                <p>
+                    Fold
+                </p>
+            </button>
+            <button className="actionButton yellowBack">
+                <p>
+                    Bet 1/2
+                </p>
+            </button>
+            <button className="actionButton yellowBack">
+                <p>
+                    Bet 3/4
+                </p>
+            </button>
+            <button className="actionButton yellowBack">
+                <p>
+                    Bet Pot
+                </p>
+            </button>
+            <button className="actionButton yellowBack">
+                <p>
+                    Rip It
+                </p>
+            </button>
+            <button className="actionButton yellowBack">
+                <p>
+                    Bet
+                </p>
+            </button>
+            <form className="actionButton long">
+                    <input 
+                    className="input yellowBack"
+                    type="text" 
+                    name="name" 
+                    value={this.state.bet} 
+                    onChange={this.handleChange}/>
+            </form>
         </div>
     )}
 }
